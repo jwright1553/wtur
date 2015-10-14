04ec9969a1fa2cf752a72191e2af850faa9e451d
jordancwright <jordancwright314@gmail.com>
2015-10-14 16:46:58 -0400

    Adding files for rounded corners

--- app/src/main/java/com/example/jordanwright/wtur/About_Fragment.java
+++ app/src/main/java/com/example/jordanwright/wtur/About_Fragment.java
@@ -4,15 +4,83 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 
 /**
  * Created by jordan on 10/14/15.
  */
 public class About_Fragment extends android.support.v4.app.Fragment {
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        super.onCreateView(inflater, container, savedInstanceState);
+
+
+
+
         final View aboutView = inflater.inflate(R.layout.about_layout, container, false);
+
+        // get the listview
+        expListView = (ExpandableListView) aboutView.findViewById(R.id.expandableListView);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(getActivity().getApplicationContext(), listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
         return aboutView;
+
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Monday");
+        listDataHeader.add("Tuesday");
+        listDataHeader.add("Wednesday");
+        listDataHeader.add("Thursday");
+        listDataHeader.add("Friday");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("8pm: A Day From Sunday");
+        top250.add("9pm: Echoes");
+        top250.add("9:30pm DJ Emily Nosek");
+
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
     }
 
 }
